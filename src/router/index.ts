c82644f5c39e26bc85699387ffe0881b05340471
mj2068 <yuelianghushenfu@sina.com>
2023-12-18 17:51:30 +0800

    change HomeView to FilmListView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
+import FilmListView from '@/views/FilmListView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import FilmDetailView from '../views/FilmDetailView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,17 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/film',
+      name: 'film',
+      component: FilmListView,
+    },
+    {
+      path: '/film/:id(\\d+)',
+      name: 'filmDetail',
+      component: FilmDetailView,
+      props: true,
     },
   ],
 });
---
+++ src/views/FilmDetailView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import { useRequest } from 'vue-request';
+
+/** ************ types ************ */
+
+// props from route(props: true)
+const props = defineProps<{
+  id: string;
+}>();
+
+console.log(props.id);
+</script>
